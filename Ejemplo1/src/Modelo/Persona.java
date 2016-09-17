0d4dcb69c2b372432c71ec03c67e178a0384a0cf
NicolasDuque <nikoduke97@gmail.com>
2016-09-16 19:03:26 -0500

    Creacion variables persona
    Creacion de variables (constructor, getter y setters)
    
    tipo:Funcionalidad

--- Ejemplo1/src/Modelo/Persona.java
+++ Ejemplo1/src/Modelo/Persona.java
@@ -10,5 +10,59 @@ package Modelo;
  * @author Nicolas
  */
 public class Persona {
+    private int cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+
+    public Persona(int cedula, String nombre, String apellido, int edad, String genero) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
     
 }
